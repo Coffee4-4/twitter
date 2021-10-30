5835aa3552b9b1a919a725988c21af84903d7458
André Luiz Melicio <71500309+Coffee4-4@users.noreply.github.com>
2021-10-29 21:53:46 -0300

    29-10-21

--- App/route.php
+++ App/route.php
@@ -1,53 +1,23 @@
 <?php
-    namespace App;
+namespace App;
+use MF\init\Bootstrap;
 
-    class route{
-        private $routes;
+class route extends Bootstrap{
 
-        public function __construct()
-        {
-            $this->initRoutes();
-            $this->run($this->getUrl());
-        }
+    protected function initRoutes(){
+        $routes['home'] = array(
+            'route' => '/',
+            'controller' => 'indexController',
+            'action' => 'index'
+        );
 
-        public function getRoutes(){
-            return $this->routes;
-        }
-        public function setRoutes(array $routes){
-            $this->routes = $routes;
-        }
+        $routes['sobre_nos'] = array(
+            'route' => '/sobreNos',
+            'controller' => 'indexController',
+            'action' => 'sobreNos'
+        );
 
-        public function initRoutes(){
-            $routes['home'] = array(
-                'route' => '/',
-                'controller' => 'indexController',
-                'action' => 'index'
-            );
-
-            $routes['sobre_nos'] = array(
-                'route' => '/sobreNos',
-                'controller' => 'indexController',
-                'action' => 'sobreNos'
-            );
-
-            $this->getRoutes($routes);
-        }
-
-        public function run($url){
-            foreach($this->getRoutes() as $key => $route){
-                if($url == $route['route']){
-                    $class = "App\\Controllers\\" .ucfirst($route['controller']);
-
-                    $controller = new $class;
-
-                    $action = $route['action'];
-                    $controller->$action();
-                }
-            }
-        }
-
-        public function getUrl(){
-            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // SuperGlobal
-        }
+        $this->setRoutes($routes);
     }
+}
 ?>
